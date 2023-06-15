2413d099f57a81979ed3fa10e360e002e8f67576
alvseven <alv7contato@outlook.com>
2023-06-15 17:16:04 -0300

    adding git ignore

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -7,37 +7,72 @@ public class Calculator {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		System.out.println("Caculo da Soma");
+		System.out.println("Sum calculation");
+		System.out.println();
 		Number sumResult = Calculator.sum(scan);
-		System.out.println("O resultado da soma é:" + sumResult);
+		System.out.println("The result of the sum calculation is: " + sumResult);
 
 		System.out.println();
-		System.out.println();
 
-		System.out.println("Caculo da Subtração");
+		System.out.println("Subtraction calculation");
+		System.out.println();
 		Number subtractResult = Calculator.subtract(scan);
-		System.out.println("O resultado da subtração é:" + subtractResult);
+		System.out.println("The result of the subtraction calculation is: " + subtractResult);
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Multiplication calculation");
+		Number multiplyResult = Calculator.multiply(scan);
+		System.out.println("The result of the multiplication calculation is: " + multiplyResult);
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Division calculation");
+		Number divideResult = Calculator.divide(scan);
+		System.out.println("The result of the division calculation is: " + divideResult);
 		
 		scan.close();
 	}
 	
 	public static Number sum(Scanner scan) {
-		System.out.println("Digite um número:");
+		System.out.println("Type a number:");
 		Float firstValue = scan.nextFloat();
 		
-		System.out.println("Digite um número:");
+		System.out.println("Type another number:");
 		Float secondValue = scan.nextFloat();
 		
 		return firstValue + secondValue;
 	}
 	
 	public static Number subtract(Scanner scan) {
-		System.out.println("Digite um número");
+		System.out.println("Type a number:");
 		Float firstValue = scan.nextFloat();
 		
-		System.out.println("Digite outro número");
+		System.out.println("Type another number:");
 		Float secondValue = scan.nextFloat();
-		
+	    	
 		return firstValue - secondValue;
+	}
+	
+	public static Number multiply(Scanner scan) {
+		System.out.println("Type a number:");
+		Float firstValue = scan.nextFloat();
+		
+		System.out.println("Type another number:");
+		Float secondValue = scan.nextFloat();
+	    	
+		return firstValue * secondValue;
+	}
+	
+	public static Number divide(Scanner scan) {
+		System.out.println("Type a number:");
+		Float firstValue = scan.nextFloat();
+		
+		System.out.println("Type another number:");
+		Float secondValue = scan.nextFloat();
+	    	
+		return firstValue / secondValue;
 	}
 }
